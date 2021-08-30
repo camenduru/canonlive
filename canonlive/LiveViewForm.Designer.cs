bc90e3c839e1ebabb271a225268258cd434da0bf
camenduru <camenduru@gmail.com>
2021-08-30 14:05:38 +0300

    Add project files.

---
+++ canonlive/LiveViewForm.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace canonlive
+{
+    partial class LiveViewForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.LiveBox1 = new System.Windows.Forms.PictureBox();
+            this.LiveRefreshTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.LiveBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // LiveBox1
+            // 
+            this.LiveBox1.Location = new System.Drawing.Point(0, 0);
+            this.LiveBox1.Margin = new System.Windows.Forms.Padding(0);
+            this.LiveBox1.Name = "LiveBox1";
+            this.LiveBox1.Size = new System.Drawing.Size(960, 640);
+            this.LiveBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.LiveBox1.TabIndex = 0;
+            this.LiveBox1.TabStop = false;
+            this.LiveBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.LiveBox1_MouseClick);
+            // 
+            // LiveRefreshTimer
+            // 
+            this.LiveRefreshTimer.Interval = 60;
+            this.LiveRefreshTimer.Tick += new System.EventHandler(this.LiveRefreshTimer_Tick);
+            // 
+            // LiveViewForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(960, 640);
+            this.Controls.Add(this.LiveBox1);
+            this.Name = "LiveViewForm";
+            this.Text = "canonlive";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.LiveViewForm_FormClosing);
+            this.Load += new System.EventHandler(this.LiveViewForm_Load);
+            this.ResizeEnd += new System.EventHandler(this.LiveViewForm_Resize);
+            this.Resize += new System.EventHandler(this.LiveViewForm_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.LiveBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox LiveBox1;
+        private System.Windows.Forms.Timer LiveRefreshTimer;
+    }
+}
---
+++ canonlive/LiveViewForm.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace canonlive
+{
+    public partial class LiveViewForm : Form
+    {
+
+        IntPtr cameraListRef = new IntPtr();
+        IntPtr[] camera = new IntPtr[1];
+        int cameraCount;
+
+        public LiveViewForm()
+        {
+            InitializeComponent();
+        }       
+
+        private void LiveViewForm_Load(object sender, EventArgs e)
+        {
+            cameraControl.InitCamera();
+            cameraListRef = cameraControl.GetCameras(out cameraCount);
+            camera = cameraControl.OpenCameras(cameraListRef, cameraCount);
+            for (int i = 0; i < cameraCount; i++)
+            {
+                cameraControl.LiveView(camera[i]);
+            }
+            LiveRefreshTimer.Start();
+            this.Text = "canon live | v0.1 | dev:camenduru | " + LiveBox1.Width + "x" + LiveBox1.Height;
+        }
+
+        private void LiveRefreshTimer_Tick(object sender, EventArgs e)
+        {
+            LiveBox1.Image = cameraControl.DownloadEvf(camera[0]);
+        }
+
+        private void LiveViewForm_Resize(object sender, EventArgs e)
+        {
+            Control control = (Control)sender;
+            LiveBox1.Width = control.Size.Width;
+            LiveBox1.Height = ((2 * LiveBox1.Width) / 3);
+            this.Text = "canon live | v0.1 | dev:camenduru | " + LiveBox1.Width + "x" + LiveBox1.Height;
+        }
+
+        private void LiveViewForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            cameraControl.CloseCameras(camera, cameraCount);
+        }
+
+        private void LiveBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                this.Text = "canon live | v0.1 | dev:camenduru | " + LiveBox1.Width + "x" + LiveBox1.Height + " | Focus Far";
+                cameraControl.FocusOFF(camera, cameraCount);
+                cameraControl.FocusFar3(camera, cameraCount);
+            }
+            else if (e.Button == MouseButtons.Left)
+            {
+                this.Text = "canon live | v0.1 | dev:camenduru | " + LiveBox1.Width + "x" + LiveBox1.Height + " | Focus Near";
+                cameraControl.FocusOFF(camera, cameraCount);
+                cameraControl.FocusNear3(camera, cameraCount);
+            }
+            else if (e.Button == MouseButtons.Middle)
+            {
+                this.Text = "canon live | v0.1 | dev:camenduru | " + LiveBox1.Width + "x" + LiveBox1.Height + " | Focus AF";
+                cameraControl.FocusOFF(camera, cameraCount);
+                cameraControl.FocusON(camera, cameraCount);
+            }
+        }
+
+    }
+}
